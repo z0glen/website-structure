196937d64bcb7fefa204dbc6701d651bc0d32607
z0glen <zgbauer@gmail.com>
2019-01-17 14:12:17 -0500

    fix(app): remove flask freezer

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_frozen import Freezer
 
 app = Flask(__name__)
 
 Bootstrap(app)
-freezer = Freezer(app)
 
 @app.route('/')
 def home():
@@ -16,5 +14,4 @@ def bio():
 	return render_template('bio.html')
 
 if __name__ == '__main__':
-    freezer.freeze();
     app.run()
